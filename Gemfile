7aa0b9436f3a02fa4e6b11a7af69a4b6e83c74a7
Partha Sundaram <parthasundaram@gmail.com>
2014-08-17 10:20:37 -0700

    Add Bootstrap

--- Gemfile
+++ Gemfile
@@ -4,6 +4,8 @@ ruby '2.1.2'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 
 gem 'rails', '4.1.4'
+gem 'bootstrap-sass', '~> 3.2.0'
+gem 'autoprefixer-rails'
 
 # Use sqlite3 as the database for Active Record
 group :production do
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  get 'about' => 'pages#about'
+
+  root :to => 'pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
